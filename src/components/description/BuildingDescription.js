b4179413347220843019aeb72e54ed17c6347c73
KiviKong <kevin95ra@gmail.com>
2018-05-01 15:49:39 -0500

    Arregla el click en las tarjetas de edificios

--- src/components/description/BuildingDescription.js
+++ src/components/description/BuildingDescription.js
@@ -11,9 +11,16 @@ export class BuildingDescription extends React.Component {
   }
 
   handleOnClick = (e) => {
-    console.log(e.target);
     if (e.target.className === 'gridBuildingLabels') 
         this.props.onClick(elementos.get(e.target.textContent.charAt(0)));
+  }
+
+  handleOnClickH2 = (e) => {
+    this.props.onClick(elementos.get(e.target.innerText));
+  }
+
+  handleOnClickP = (e) => {
+    this.props.onClick(elementos.get(e.target.id));
   }
 
   createBuildings = () => {
@@ -23,13 +30,15 @@ export class BuildingDescription extends React.Component {
         buildings.push(
             (
                 <div key={i} className={'building_' + (i+1)}>
-                    <div className="gridBuildingLabels" onClick={this.handleOnClick}>
+                    <div className="gridBuildingLabels"  onClick={this.handleOnClick}>
                         <div className="BuildingLabels_name">
-                            <div className="BuildingLabels_circle">
-                                <h2>{edificios[i].nombre}</h2>
+                            <div className="BuildingLabels_circle" onClick={this.handleOnClickH2}>
+                                <h2 >{edificios[i].nombre}</h2>
                             </div>
                         </div>
-                        <div className="BuildingLabels_description"><p>{edificios[i].resumen}</p></div>
+                        <div className="BuildingLabels_description">
+                            <p id={edificios[i].nombre} onClick={this.handleOnClickP}>{edificios[i].resumen}</p>
+                        </div>
                     </div>
                 </div>
             )
