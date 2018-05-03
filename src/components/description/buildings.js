514033251642311371849b0c49277599b2ddac3f
KiviKong <kevin95ra@gmail.com>
2018-05-03 09:17:52 -0500

    cambia la descripcion de los edificios a español

--- src/components/description/buildings.js
+++ src/components/description/buildings.js
@@ -2,35 +2,35 @@ export const edificios = [
     {
         nombre: "A",
         resumen: "Biblioteca",
-        descripcion: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        descripcion: "Yo no soy un producto de mis circunstancias. Soy un producto de mis decisiones. -Stephen Covey",
         coordenadas: [[2,2],[2,3],[2,4],[3,2],[3,3],[3,4],[4,2],[4,3],[4,4]],
         img: 'https://www.brunoticias.com/wp-content/uploads/2018/01/ITESO-campus-640x367.jpg'
     },
     {
         nombre: "B",
         resumen: "Auditorio",
-        descripcion: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+        descripcion: "Cada niño es un artista. El problema es cómo seguir siendo artista una vez que se crece. -Pablo Picasso",
         coordenadas: [[7,1],[7,2],[8,1],[8,2],[9,1],[9,2]],
         img: 'https://static.wixstatic.com/media/fcbae3_23198ab168134c74b4f477676fd2c7ed~mv2.jpg/v1/fill/w_675,h_338,al_c,q_80,usm_0.66_1.00_0.01/fcbae3_23198ab168134c74b4f477676fd2c7ed~mv2.jpg'
     },
     {
         nombre: "C",
         resumen: "Cafetería",
-        descripcion: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+        descripcion: "Nunca se puede cruzar el océano hasta que tenga el coraje de perder de vista la costa. -Cristóbal Colón",
         coordenadas: [[13,2],[13,3],[13,4],[13,5],[13,6],[14,2],[14,3],[14,4],[14,5],[14,6]],
         img: 'https://res-5.cloudinary.com/abroad101/image/upload/c_fill,h_270,w_370/v1442872331/Program_13084_ProfilePhoto__2571.jpg'
     },
     {
         nombre: "D",
         resumen: "Aula de clases ",
-        descripcion: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        descripcion: "He aprendido que la gente olvidará lo que dijiste, la gente olvidará lo que hiciste, pero las personas nunca olvidarán cómo los hiciste sentir. -Maya Angelou",
         coordenadas: [[17,2],[17,3],[17,4],[17,5],[17,6],[18,2],[18,3],[18,4],[18,5],[18,6]],
         img: 'https://res-2.cloudinary.com/abroad101/image/upload/c_limit,h_800,w_800/v1442871532/Program_13084_Photo__aafa.jpg'
     },
     {
         nombre: "E",
         resumen: "Aulas de audiovisuales",
-        descripcion: "In three words I can sum up everything I've learned about life: it goes on.",
+        descripcion: "Tanto si piensas que puedes o piensas que no puedes, tienes razón. -Henry Ford",
         coordenadas: [[15,15],[15,16],[15,17],[15,18],[15,19],[16,15],[16,16],[16,17],[16,18],[16,19],[17,15],[17,16],[17,17],[17,18],[17,19],[18,15],[18,16],[18,17],[18,18],[18,19]],
         img: 'https://res-1.cloudinary.com/abroad101/image/upload/c_limit,h_800,w_800/v1442871516/Program_13084_Photo__5dda.jpg'
         
@@ -45,21 +45,21 @@ export const edificios = [
     {
         nombre: "G",
         resumen: "Edificio de humanidades",
-        descripcion: "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+        descripcion: "Los dos días más importantes en su vida son los días que se nace y el día que se descubre por qué. -Mark Twain",
         coordenadas: [[9,17],[9,18],[10,17],[10,18]],
         img: 'http://jesuitasaru.org/wp-content/uploads/2015/09/foto01_0.jpg'
     },
     {
         nombre: "H",
         resumen: "Edificio de ingeniería",
-        descripcion: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+        descripcion: "Lo que puedas hacer o soñar, ponte a hacerlo. La osadía está llena de genialidad, poder y magia. -Johann Wolfgang von Goethe",
         coordenadas: [[7,8],[7,9],[7,10],[7,11],[7,12],[8,8],[8,9],[8,10],[8,11],[8,12],[9,8],[9,9],[9,10],[9,11],[9,12]],
         img: 'http://www.circuloblanco.com/wp-content/uploads/2015/08/CirculoBlanco_Proyectos_LABcomunicacionITESO-27.jpg'
     },
     {
         nombre: "K",
         resumen: "Edificio de /n lenguas",
-        descripcion: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        descripcion: "La gente suele decir que la motivación no dura mucho. Bueno, tampoco lo hace la ducha -por eso la recomendamos a diario. -Zig Ziglar",
         coordenadas: [[2,16],[2,17],[2,18],[2,19],[3,16],[3,17],[3,18],[3,19],[4,16],[4,17],[4,18],[4,19]],
         img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/BibliotecaITESO.jpg/654px-BibliotecaITESO.jpg'
     },
